a6cc6b2696d02aec9aac3c1ac18d3f909a16bdba
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-04-21 13:04:44 +0530

    Added Exception Handling for wrong input (non-existant location)

--- weather_details/views.py
+++ weather_details/views.py
@@ -7,25 +7,42 @@ def home(request):
     return render(request, 'core/home.html', {"page": "Home"})
 
 def details(request):
+    location = request.GET['search_input']
+    today = datetime.date.today()
+    days = [(today - datetime.timedelta(days=i)).strftime('%d/%m') for i in range(1, 5)]
+    
     try:
-        location = request.GET['search_input']
+        lat = request.GET["lat"]
+        lng = request.GET["lng"]
+
     except:
-        pass
+        return render(request, 'weather/weatherdetail.html', {
+                            "curr_max_temp": "-",
+                            "curr_min_temp": "-",
+                            "curr_precipitation": "-",
+                            "curr_humidity": "-",
+                            "forecast": list(
+                                    zip(
+                                        [0 for _ in range(4)], 
+                                        [(today + datetime.timedelta(days=i)).strftime('%d/%m') for i in range(1, 5)]
+                                    )
+                                ),
+                            "page": "Detail", 
+                            "temp_high": [0 for _ in range(7)],
+                            "temp_low": [0 for _ in range(7)], 
+                            "precipitation": [0 for _ in range(7)],
+                            "humidity": [0 for _ in range(7)],
+                            "days": days, 
+                            "location": location
+                        })
 
     curr_max_temp = 30
     curr_min_temp = 20
     curr_precipitation = 50
     curr_humidity = 40
     
-    today = datetime.date.today()
-    
     forecast_data = [(5, 2), (5, 3), (6, 3), (7, 1)]
     forecast = list(zip(forecast_data, [(today + datetime.timedelta(days=i)).strftime('%d/%m') for i in range(1, 5)]))
-
-    days = []
-
-    for i in range(1, 8):
-        days.append((today - datetime.timedelta(days=i)).strftime('%d/%m'))
 
     temp_high = [5, 5, 6, 7, 4, 6, 4.2]
     temp_low = [2, 1, 2, 3, 2, 1, 1.2]
