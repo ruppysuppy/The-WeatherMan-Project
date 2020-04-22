c7fbad67b5989795ea0aad4d1389824820aa8a16
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-04-22 16:43:23 +0530

    Completed previous weather data

--- weather_details/views.py
+++ weather_details/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-import datetime
-import requests as rq
-
 from geopy.geocoders import Nominatim
 
-API_END_PT = "https://api.openweathermap.org/data/2.5/onecall?lat={:.1f}&lon={:.1f}&appid={}&units=metric"
+import datetime
+import requests as rq
+from time import mktime
+
+API_END_PT_CURR_FORCAST = "https://api.openweathermap.org/data/2.5/onecall?lat={:.1f}&lon={:.1f}&appid={}&units=metric"
+API_END_PT_HISTORIC_DATA = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={:.1f}&lon={:.1f}&dt={}&appid={}&units=metric"
 API_KEY = "d496fded6f52e05a233d961633e95207"
 IMAGE_URL = "http://openweathermap.org/img/wn/{}@2x.png"
 locator = Nominatim(user_agent="weatherManGeoCoder")
@@ -28,7 +30,7 @@ def details(request):
                             "curr_humidity": "-",
                             "forecast": list(
                                     zip(
-                                        [0 for _ in range(4)], 
+                                        [("-", "-", "-", "-", "-") for _ in range(3)], 
                                         [(today + datetime.timedelta(days=i)).strftime('%d/%m') for i in range(1, 5)]
                                     )
                                 ),
@@ -43,7 +45,7 @@ def details(request):
                         })
     
     try:
-        url = API_END_PT.format(latitude, longitude, API_KEY)
+        url = API_END_PT_CURR_FORCAST.format(latitude, longitude, API_KEY)
         response = rq.get(url=url).json()
     except:
         return render(request, 'weather/weatherdetail.html', {
@@ -52,7 +54,7 @@ def details(request):
                             "curr_humidity": "-",
                             "forecast": list(
                                     zip(
-                                        [0 for _ in range(4)], 
+                                        [("-", "-", "-", "-", "-") for _ in range(3)], 
                                         [(today + datetime.timedelta(days=i)).strftime('%d/%m') for i in range(1, 5)]
                                     )
                                 ),
@@ -97,10 +99,48 @@ def details(request):
 
     forecast = list(zip(forecast_data, [(today + datetime.timedelta(days=i)).strftime('%d/%m') for i in range(1, 4)]))
 
-    temp_high = [5, 5, 6, 7, 4, 6, 4.2]
-    temp_low = [2, 1, 2, 3, 2, 1, 1.2]
-    precipitation = [0, 0, 0, 0, 0, 0, 0.5]
-    humidity = [50, 60, 55, 40, 43.2, 30, 50]
+    data_dict = {(today - datetime.timedelta(days=i)): [0, 9999, 0, 0] for i in range(1, 7)}
+    days = [key.strftime("%d/%m") for key in data_dict][:-2]
+
+    for key in data_dict:
+        url = API_END_PT_HISTORIC_DATA.format(latitude, longitude, int(mktime(key.timetuple())), API_KEY)
+
+        response_temp = rq.get(url=url)
+        response = response_temp.json()
+        response_code = response_temp.status_code
+
+        rain = 0
+        humidity_temp = []
+        
+        if (response_code != 400):
+            for data in response["hourly"]:
+                time = datetime.date.fromtimestamp(data["dt"])
+
+                if (data_dict[time][0] < data["temp"]):
+                    data_dict[time][0] = data["temp"]
+                if (data_dict[time][1] > data["temp"]):
+                    data_dict[time][1] = data["temp"]
+                
+                try:
+                    rain += data["rain"]
+                except:
+                    pass
+
+                humidity_temp.append(data["humidity"])
+        else:
+            data_dict[key] = [0, 0, 0, 0]
+        
+        data_dict[key][2] = rain
+
+        try:
+            data_dict[key][3] = sum(humidity_temp) / len(humidity_temp)
+        except ZeroDivisionError:
+            data_dict[key][3] = 0
+
+    temp_high = [data_dict[key][0] for key in data_dict][:-2] # [5, 5, 6, 7, 4, 6, 4.2]
+    temp_low = [data_dict[key][1] for key in data_dict][:-2] # [2, 1, 2, 3, 2, 1, 1.2]
+    precipitation = [data_dict[key][2] for key in data_dict][:-2] # [0, 0, 0, 0, 0, 0, 0.5]
+    humidity = [data_dict[key][3] for key in data_dict][:-2] # [50, 60, 55, 40, 43.2, 30, 50]
 
     return render(request, 'weather/weatherdetail.html', {
                             "curr_temp": curr_temp,
